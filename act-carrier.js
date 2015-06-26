022bbe5cb859ca302d5b06ddf4716489e54f05a1
Carl Olsen <unstoppablecarlolsen@gmail.com>
2015-06-26 14:22:52 -0400

    no message

--- act-carrier.js
+++ act-carrier.js
@@ -3,7 +3,11 @@
 // carries energy
 var role = {
     init: false,
-    act: false
+    act: function(creep) {
+        if (creep.idle() && creep.energy === creep.energyCapacity) {
+            creep.startTask('energy_store');
+        }
+    },
 };
 
 module.exports = role;
