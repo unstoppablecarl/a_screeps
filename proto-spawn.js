cf5ba3b9b7881c17b8bb26d964452a4897fa5fca
Carl Olsen <unstoppablecarlolsen@gmail.com>
2015-06-26 14:49:36 -0400

    no message

--- proto-spawn.js
+++ proto-spawn.js
@@ -6,25 +6,27 @@ Spawn.prototype.spawnCreep = function(body, memory) {
     memory.spawn_id = this.id;
 
     var result = this.createCreep(body, null, memory);
-    if (result !== OK) {
+        var error;
         if(result === ERR_NOT_ENOUGH_ENERGY){
             // do nothing
             return result;
             // result = 'ERR_NOT_ENOUGH_ENERGY';
         }
         else if(result === ERR_NOT_OWNER){
-            result = 'ERR_NOT_OWNER';
+            error = 'ERR_NOT_OWNER';
         }
         else if(result === ERR_NAME_EXISTS){
-            result = 'ERR_NAME_EXISTS';
+            error = 'ERR_NAME_EXISTS';
         }
         else if(result === ERR_BUSY){
-            result = 'ERR_BUSY';
+            error = 'ERR_BUSY';
         }
         else if(result === ERR_INVALID_ARGS){
-            result = 'ERR_INVALID_ARGS';
+            error = 'ERR_INVALID_ARGS';
         }
-        console.log('SPAWN ERROR', this.room.name, this.name, 'cannot create', memory.role, result);
+
+    if(error){
+        console.log('SPAWN ERROR', this.room.name, this.name, 'cannot create', memory.role, error);
     } else {
         console.log(this.name, 'spawning', result, memory.role);
     }
