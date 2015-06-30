f97b1fd45aa53a134e9938e93d5e506dfa9d42b0
Carl Olsen <unstoppablecarlolsen@gmail.com>
2015-06-30 14:34:30 -0400

    x

--- job-manager.js
+++ job-manager.js
@@ -357,7 +357,7 @@ JobManager.prototype = {
         // if(!creep){
         //     return false;
         // }
-        console.log(JSON.stringify(creep));
+        console.log(JSON.stringify(creep), JSON.stringify(job));
         this.assignNewJob(creep, job);
 
         console.log('allocate job ', job.type, creep.name);
