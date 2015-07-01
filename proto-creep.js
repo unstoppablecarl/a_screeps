fbf58f2ee41edec5565ac01e8369d5f27b82dd18
Carl Olsen <unstoppablecarlolsen@gmail.com>
2015-07-01 13:00:39 -0400

    x

--- proto-creep.js
+++ proto-creep.js
@@ -14,7 +14,7 @@ Creep.prototype.act = function() {
     var role = this.role();
     var roleHandler = roles[role];
     if(!roleHandler){
-        console.log("ERRROR role handler not found");
+        console.log("ERRROR role handler not found for role: " + role);
         return;
     }
     if (this.memory.pending_creation){
