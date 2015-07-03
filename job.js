667d2962ed23308c1d5940a724bd7002e7ff7b6c
Carl Olsen <unstoppablecarlolsen@gmail.com>
2015-07-03 13:37:29 -0400

    x

--- job.js
+++ job.js
@@ -254,7 +254,7 @@ Job.prototype = {
     },
 
     toString: function(){
-        return ['Job#' + this.id(), this.type(), this.source(), this.target()].join(' ');
+        return ['[Job#' + this.id(), this.type(), this.source(), this.target(), ']'].join(' ');
     },
 };
 
