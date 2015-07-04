815d36e290864818a6aca386ed7ece0913c53f8b
Carl Olsen <unstoppablecarlolsen@gmail.com>
2015-07-03 20:03:39 -0400

    x

--- job-build.js
+++ job-build.js
@@ -3,16 +3,18 @@
 var job_build = {
     name: 'build',
     start: false,
-    act: function(creep){
-        var target = creep.taskTarget();
+    act: function(creep, job){
+        var target = job.target();
 
         if(!target){
-            creep.endTask();
+            job.end();
         }
 
         if(target){
-            creep.moveTo(target);
-            creep.build(target);
+            var result = creep.build(target);
+            if(result === ERR_NOT_IN_RANGE){
+                creep.moveTo(target);
+            }
         }
     },
     cancel: false,
