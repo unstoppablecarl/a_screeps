da90ad5e6f69efd139c51376653c16414f857e7a
Carl Olsen <unstoppablecarlolsen@gmail.com>
2015-07-04 20:00:14 -0400

    x

--- job-manager.js
+++ job-manager.js
@@ -581,7 +581,7 @@ JobManager.prototype = {
         list.addMultiple(newJobs);
         this.prioritizeJobs(list.all());
 
-        this.report();
+        // this.report();
     },
     reportData: function(jobs) {
         var jobData = [];
