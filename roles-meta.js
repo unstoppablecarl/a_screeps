0c3ea92cdc22fa20f2cf96e9d4ff376b216f93e0
Carl Olsen <unstoppablecarlolsen@gmail.com>
2015-07-13 20:43:20 -0400

    x

--- roles-meta.js
+++ roles-meta.js
@@ -162,6 +162,12 @@ TOUGH:          10
                     WORK, WORK, WORK, WORK,
                     MOVE, MOVE, MOVE, MOVE
                 ],
+
+                [
+                    CARRY, CARRY, CARRY, CARRY, CARRY,
+                    WORK, WORK, WORK, WORK, WORK,
+                    MOVE, MOVE, MOVE, MOVE, MOVE
+                ],
             ]
         },
         carrier: {
